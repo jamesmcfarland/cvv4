1369fdcc1756095df7b3fd5cff159ce593f3775f
James McFarland <24841376+jamesmcfarland@users.noreply.github.com>
2025-07-21 01:12:16 +0100

    new landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,68 @@
 import AboutSection from "@/components/sections/about";
 import SeeMoreButton from "@/components/seeMoreButton";
 import { LightDarkModeToggle } from "@/components/theme-picker";
-import { GeistMono } from "geist/font";
+import { GeistMono } from "geist/font/mono";
 import Nav from "@/components/nav";
 import ProjectsSection from "@/components/sections/projects";
 import { Separator } from "@/components/ui/separator";
 import ContactSection from "@/components/sections/contact";
+import TextType from "@/TextType/TextType";
+import TextureImage from "@/content/images/texture.jpg";
 
 export default function Home() {
   return (
     <main>
-      <div className="h-screen w-full flex flex-col justify-between p-8">
-        <div className="self-end">
+      <div className="h-screen w-full flex flex-col justify-between p-8 relative">
+        {/* Texture image overlay with 10% opacity */}
+        <div
+          className="absolute inset-0"
+          style={{
+            backgroundImage: `url(${TextureImage.src})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            opacity: 0.07,
+            zIndex: 0,
+            pointerEvents: "none",
+          }}
+        />
+
+        <div className="relative z-10 self-end">
           <LightDarkModeToggle />
         </div>
-        <div>
+        <div className="relative z-10">
           <div
             className={`${GeistMono.className} uppercase tracking-widest opacity-90 mb-4`}
           >
-            <h1>1st class software engineering @ qub</h1>
-            <h1>former intern @ libertyit</h1>
-            <h1>vice president @ queens&apos; computing society</h1>
-            <h1>secure by design</h1>
-            <h1>f1 fan</h1>
+            <TextType
+              text={[
+                "1st class software engineering @ qub",
+                "former intern @ libertyit",
+                "vice president @ queens' computing society",
+                "security focussed",
+                "f1 fan",
+              ]}
+              pauseDuration={2000}
+              cursorCharacter="_"
+              className="text-black"
+            />
           </div>
           <h1 className="text-4xl sm:text-6xl font-bold lowercase">
             jamesmcfarland
           </h1>
+          <TextType
+            text={[
+              "1st class software engineering @ qub",
+              "former intern @ libertyit",
+              "vice president @ queens' computing society",
+              "security focussed",
+              "f1 fan",
+            ].reverse()}
+            pauseDuration={2000}
+            cursorCharacter="_"
+            className={`uppercase opacity-90 mt-4 ${GeistMono.className}`}
+          />
         </div>
-        <div className="justify-self-end">
+        <div className="relative z-10 justify-self-end">
           <SeeMoreButton />
         </div>
       </div>
